36e215abaaa9237632b77d2bcd766966d339e9df
ikumar07 <ishwar.kumar@anko.com>
2024-04-05 12:08:43 +0530

    added image icons

---
+++ fairshare/lib/common/icon_images.dart
@@ -0,0 +1,45 @@
+class IconImages {
+  static const burger = 'lib/assets/icon_images/burger.png';
+  static const beerMug = 'lib/assets/icon_images/beer_mug.png';
+  static const coffee = 'lib/assets/icon_images/coffee.png';
+  static const currency = 'lib/assets/icon_images/currency.png';
+  static const houseRent = 'lib/assets/icon_images/rent.png';
+  static const meal = 'lib/assets/icon_images/meal.png';
+  static const movie = 'lib/assets/icon_images/movie.png';
+  static const person = 'lib/assets/icon_images/person.png';
+  static const swimming = 'lib/assets/icon_images/swimming.png';
+  static const tickets = 'lib/assets/icon_images/ticket.png';
+  static const transactionHistory = 'lib/assets/icon_images/transaction.png';
+  static const beach = 'lib/assets/icon_images/beach.png';
+  static const bill = 'lib/assets/icon_images/bill.png';
+  static const wifi = 'lib/assets/icon_images/wifi.png';
+  static const noodles = 'lib/assets/icon_images/noodles.png';
+  static const pizza = 'lib/assets/icon_images/pizza.png';
+  static const grocery = 'lib/assets/icon_images/grocery.png';
+  static const cigarette = 'lib/assets/icon_images/cigarette.png';
+  static const shopping = 'lib/assets/icon_images/shopping.png';
+  static const petrol = 'lib/assets/icon_images/petrol.png';
+
+  static const List<String> images = [
+    burger,
+    beerMug,
+    coffee,
+    currency,
+    houseRent,
+    meal,
+    movie,
+    person,
+    swimming,
+    tickets,
+    transactionHistory,
+    beach,
+    bill,
+    wifi,
+    noodles,
+    pizza,
+    grocery,
+    cigarette,
+    shopping,
+    petrol,
+  ];
+}
